eb4d5af77323463dfa7fb057c00e89a7ab426c1d
Jorge Antônio <jorgeants@gmail.com>
2020-01-12 17:57:12 -0300

    Finish react-navigation configuration

---
+++ src/routes.js
@@ -0,0 +1,26 @@
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import Home from './pages/Home';
+import UserDetails from './pages/UserDetails';
+
+const Routes = createAppContainer(
+  createStackNavigator(
+    {
+      Home,
+      UserDetails,
+    },
+    {
+      defaultNavigationOptions: {
+        headerTitleAlign: 'center',
+        headerBackTitleVisible: false,
+        headerStyle: {
+          backgroundColor: '#666',
+        },
+        headerTintColor: '#FFF',
+      }
+    }
+  )
+);
+
+export default Routes;
