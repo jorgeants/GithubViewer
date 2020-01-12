a8b243c04f640a4755f19683357c840694d362bc
Jorge Antônio <jorgeants@gmail.com>
2020-01-12 20:38:29 -0300

    Fix eslint rulers and enable auto format and fix on save

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,11 +2,7 @@ module.exports = {
   env: {
     es6: true,
   },
-  extends: [
-    'airbnb',
-    'prettier',
-    'prettier/react'
-  ],
+  extends: ['airbnb', 'prettier', 'prettier/react'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
@@ -19,18 +15,15 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-    'prettier',
-  ],
+  plugins: ['react', 'prettier'],
   rules: {
     'prettier/prettier': 'error',
     'react/jsx-filename-extension': [
       'warn',
       {
-        extensions: ['.jsx', '.js']
-      }
-    ]
+        extensions: ['.jsx', '.js'],
+      },
+    ],
+    'import/prefer-default-export': 'off',
   },
-  'import/prefer-default-export': 'off'
 };
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,15 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import { View, Text } from 'react-native';
 
 // import { Container } from './styles';
 
-export default class Home extends Component {
-  render() {
-    return (
-      <View>
-        <Text>Home</Text>
-      </View>
-    )
-  }
+export default function Home() {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  );
 }
